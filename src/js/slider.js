0223baabe482a64fdb6dc0294ec706b66efe213e
UwetU <grafcf@gmail.com>
2020-03-30 17:29:24 +0500

    feat: add full main page

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+import Swiper from "swiper";
+
+const swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
